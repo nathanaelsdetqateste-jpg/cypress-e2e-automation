dc58fd705cf267624e55d2f029bafdb3e8166d48
Nathan <nathanael.sdet.qa.teste@gmail.com>
2026-07-13 23:11:53 -0300

    feat: Add baseUrl

---
+++ cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  allowCypressEnv: false,
+
+  e2e: {
+    setupNodeEvents(on, config) {},
+    baseUrl: "https://automationexercise.com/",
+  },
+});
