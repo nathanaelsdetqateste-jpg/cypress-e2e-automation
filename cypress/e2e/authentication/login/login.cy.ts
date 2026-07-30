b0c9376c4ecd6c056e1b42558afdf9fcc7d4288a
Nathan <nathanael.sdet.qa.teste@gmail.com>
2026-07-30 00:04:44 -0300

    feat: Implement login test with positive scenario for valid credentials

---
+++ cypress/e2e/authentication/login/login.cy.ts
@@ -0,0 +1,29 @@
+import { validUser } from "../../../factories/user/userFactory";
+import { fillRegistrationForm } from "../../../support/helpers/registerHelper";
+
+describe("Login", () => {
+  beforeEach(() => {
+    cy.visit("/login");
+  });
+
+  describe("Positive scenarios", () => {
+    it("ST-014 - Should login successfully with valid credentials.", () => {
+      const user = validUser();
+
+      cy.get('[data-qa="signup-name"]').type(user.name);
+      cy.get('[data-qa="signup-email"]').type(user.email);
+      cy.get('[data-qa="signup-button"]').click();
+
+      fillRegistrationForm(user);
+      cy.get('[data-qa="create-account"]').click();
+      cy.get("b").should("contain.text", "Account Created!");
+
+      cy.visit("/login");
+      cy.get('[data-qa="login-email"]').type(user.email);
+      cy.get('[data-qa="login-password"]').type(user.password);
+      cy.get('[data-qa="login-button"]').click();
+
+      cy.contains("Logged in as").should("contain.text", user.name);
+    });
+  });
+});
