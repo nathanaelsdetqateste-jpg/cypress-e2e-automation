1d0525204fa2cff677aee0a8d8276a5ae6feff9b
Nathan <nathanael.sdet.qa.teste@gmail.com>
2026-07-31 19:21:05 -0300

    feat: Implement addProductToCart command for product selection and cart validation

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -23,3 +23,22 @@ Cypress.Commands.add("login", () => {
     });
   });
 });
+
+Cypress.Commands.add("addProductToCart", () => {
+  cy.get('a[href="/product_details/1"]')
+    .should("contains.text", "View Product")
+    .and("be.visible")
+    .click();
+
+  cy.get("h2").should("contains.text", "Blue Top").and("be.visible");
+
+  cy.get(".btn.btn-default.cart").should("be.visible").click();
+
+  cy.get("h4").should("contains.text", "Added!").and("be.visible");
+  cy.get('a[href="/view_cart"]')
+    .find("u")
+    .should("contain.text", "View Cart")
+    .click();
+
+  cy.url().should("include", "/view_cart");
+});
