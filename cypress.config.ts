39dd30826d8906f66ec5c2c47ac163a007870eb9
Nathan <nathanael.sdet.qa.teste@gmail.com>
2026-08-02 13:19:59 -0300

    feat: integrate cypress-mochawesome-reporter for enhanced reporting

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,26 @@
 import { defineConfig } from "cypress";
+import mochawesome from "cypress-mochawesome-reporter/plugin.js";
 
 export default defineConfig({
   allowCypressEnv: false,
 
+  reporter: "cypress-mochawesome-reporter",
+
+  reporterOptions: {
+    reportDir: "cypress/reports/mochawesome-report",
+    overwrite: false,
+    html: true,
+    json: true,
+    embeddedScreenshots: true,
+    inlineAssets: true,
+  },
+
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      mochawesome(on, config);
+
+      return config;
+    },
     baseUrl: "https://automationexercise.com/",
   },
 
