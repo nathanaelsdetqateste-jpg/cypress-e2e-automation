a20a9fad11ed9a8760f8285ce783079073a24e23
Nathan <nathanael.sdet.qa.teste@gmail.com>
2026-08-03 20:34:30 -0300

    feat: add business rules for shopping cart and login validation tests

---
+++ cypress/e2e/authentication/login/businesRules.cy.ts
@@ -0,0 +1,13 @@
+describe("Login business rules", () => {
+  beforeEach(() => {
+    cy.visit("/login");
+  });
+
+  it("BR-002 - Should not allow login with unregistered credentials", () => {
+    cy.get('[data-qa="login-email"]').type("invalid@example.com");
+    cy.get('[data-qa="login-password"]').type("wrongpassword");
+    cy.get('[data-qa="login-button"]').click();
+
+    cy.contains("Your email or password is incorrect!").should("be.visible");
+  });
+});
