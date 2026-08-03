749f3003a23742e8bf47b01d71d70b9995ad15aa
Nathan <nathanael.sdet.qa.teste@gmail.com>
2026-08-02 22:48:23 -0300

    fix: remove unnecessary clear commands in login validation tests

--- cypress/e2e/authentication/login/validation.cy.ts
+++ cypress/e2e/authentication/login/validation.cy.ts
@@ -9,21 +9,21 @@ describe("Login validation", () => {
     it("ST-015 - Should show an error when email and password are invalid", () => {
       const user = validUser();
 
-      cy.get('[data-qa="login-email"]').clear().type(user.email);
-      cy.get('[data-qa="login-password"]').clear().type(user.password);
+      cy.get('[data-qa="login-email"]').type(user.email);
+      cy.get('[data-qa="login-password"]').type(user.password);
       cy.get('[data-qa="login-button"]').click();
 
       cy.contains("Your email or password is incorrect!").should("be.visible");
     });
 
     it("ST-016 - Should show validation message when email is empty", () => {
-      cy.get('[data-qa="login-password"]').clear().type(validUser().password);
+      cy.get('[data-qa="login-password"]').type(validUser().password);
       cy.get('[data-qa="login-button"]').click();
       cy.validationMessageError('[data-qa="login-email"]');
     });
 
     it("ST-017 - Should show validation message when password is empty", () => {
-      cy.get('[data-qa="login-email"]').clear().type(validUser().email);
+      cy.get('[data-qa="login-email"]').type(validUser().email);
       cy.get('[data-qa="login-button"]').click();
       cy.validationMessageError('[data-qa="login-password"]');
     });
