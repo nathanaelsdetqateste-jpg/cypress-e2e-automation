84c3b5c35b69259163a8b523388485f81fc3dc0b
Nathan <nathanael.sdet.qa.teste@gmail.com>
2026-08-07 18:59:49 -0300

    feat: add positive test for submitting a product comment

---
+++ cypress/e2e/productComments/positive.cy.ts
@@ -0,0 +1,21 @@
+describe("Product comments", () => {
+  beforeEach(() => {
+    cy.login("loginUser");
+    cy.visit("/product_details/1");
+  });
+
+  describe("Positive scenarios", () => {
+    it("ST-001 - Add a valid comment to a product successfully", () => {
+      cy.fixture("loginUser").then((user) => {
+        cy.get('input[id="name"]').type(user.name);
+        cy.get('input[id="email"]').type(user.email);
+      });
+
+      cy.get('textarea[id="review"]').type("This is a test comment.");
+      cy.get('button[id="button-review"]')
+        .should("contain.text", "Submit")
+        .click();
+      cy.contains("Thank you for your review.").should("be.visible");
+    });
+  });
+});
