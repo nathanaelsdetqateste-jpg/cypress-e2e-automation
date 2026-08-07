5b87b20059dce7fbd9b768037c3997d7497a4d31
Nathan <nathanael.sdet.qa.teste@gmail.com>
2026-08-07 18:39:02 -0300

    feat: implement business rules tests for cart functionality

--- cypress/e2e/cart/businesRules.cy.ts
+++ cypress/e2e/cart/businesRules.cy.ts
@@ -1,11 +1,77 @@
 describe("Business Rules", () => {
   beforeEach(() => {
-    cy.visit("/cart");
+    cy.clearCookies();
+    cy.visit("/");
   });
 
-  it("BR-001 - Should not allow checkout with an empty cart", () => {});
+  let headersList = [
+    "Item",
+    "Description",
+    "Price",
+    "Quantity",
+    "Total",
+    "Action",
+  ];
 
-  it("BR-002 - Should allow checkout with items in the cart", () => {});
+  describe("Positive scenarios", () => {
+    it("BR-002 - should increase the quantity of a product in the cart", () => {
+      cy.AddToCart();
+      cy.visit("/");
+      cy.AddToCart();
+      cy.visit("/");
 
-  it("BR-003 - Should display the correct total amount for the items in the cart", () => {});
+      cy.get("a[href='/view_cart']").find(".fa.fa-shopping-cart").click();
+
+      for (let header of headersList) {
+        cy.get("#cart_info").find(".cart_menu").should("contain.text", header);
+        break;
+      }
+
+      cy.get(".disabled")
+        .invoke("text")
+        .then((quantity) => {
+          const result = Number(quantity);
+          expect(result).to.equal(2);
+        });
+    });
+
+    it("BR-003 - The system should remove a product from the cart successfully", () => {
+      cy.AddToCart();
+
+      for (let header of headersList) {
+        cy.get("#cart_info").find(".cart_menu").should("contain.text", header);
+        break;
+      }
+
+      cy.get(".cart_quantity_delete").should("be.visible").click();
+      cy.contains(/empty|carrinho vazio|cannot checkout/i).should("be.visible");
+    });
+  });
+
+  describe("Negative scenarios", () => {
+    it("BR-004 - The system should not allow checkout with an empty cart", () => {
+      cy.visit("/view_cart");
+      cy.url().should("include", "/view_cart");
+      cy.contains(/empty|carrinho vazio|cannot checkout/i).should("be.visible");
+    });
+
+    it("BR-005 - Should not add an out-of-stock product to the cart", () => {
+      cy.visit("product_details/999");
+
+      cy.get(".btn.btn-default.cart")
+        .should("contain.text", "Add to cart")
+        .and("be.visible")
+        .click();
+      cy.contains("Added!").should("not.be.visible");
+    });
+
+    it("BR-006 - should prevent checkout when user is not authenticated (redirect to login)", () => {
+      cy.AddToCart();
+
+      cy.get(".btn.btn-default.check_out").should("be.visible").click();
+      cy.contains("Register / Login account to proceed on checkout.").should(
+        "be.visible",
+      );
+    });
+  });
 });
