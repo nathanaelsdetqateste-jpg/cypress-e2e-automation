58abb9bc31e859405018c431dfeaaa77c7f77017
Nathan <nathanael.sdet.qa.teste@gmail.com>
2026-08-07 18:41:14 -0300

    feat: add positive test for adding a product to the cart

---
+++ cypress/e2e/cart/positive.cy.ts
@@ -0,0 +1,27 @@
+describe("Cart", () => {
+  beforeEach(() => {
+    cy.login("loginUser");
+    cy.visit("/");
+  });
+
+  let headersList = [
+    "Item",
+    "Description",
+    "Price",
+    "Quantity",
+    "Total",
+    "Action",
+  ];
+
+  it("BR-001 - Add a product to the cart successfully", () => {
+    cy.AddToCart();
+
+    cy.url().should("include", "/view_cart");
+    cy.get(".active").should("contain.text", "Shopping Cart").and("be.visible");
+
+    for (const header of headersList) {
+      cy.get("#cart_info").find(".cart_menu").should("contain.text", header);
+      break;
+    }
+  });
+});
