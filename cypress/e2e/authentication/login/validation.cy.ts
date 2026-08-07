36eeaa5abf8ed69e91a19a6a07b6b8f574eaf867
Nathan <nathanael.sdet.qa.teste@gmail.com>
2026-08-07 18:25:24 -0300

    refactor: update test case IDs for negative login scenarios

--- cypress/e2e/authentication/login/validation.cy.ts
+++ cypress/e2e/authentication/login/validation.cy.ts
@@ -6,7 +6,7 @@ describe("Validation", () => {
   });
 
   describe("Negative scenarios", () => {
-    it("ST-015 - Should show an error when email and password are invalid", () => {
+    it("ST-002 - Should show an error when email and password are invalid", () => {
       const user = validUser();
 
       cy.get('[data-qa="login-email"]').type(user.email);
@@ -16,13 +16,13 @@ describe("Validation", () => {
       cy.contains("Your email or password is incorrect!").should("be.visible");
     });
 
-    it("ST-016 - Should show validation message when email is empty", () => {
+    it("ST-003 - Should show validation message when email is empty", () => {
       cy.get('[data-qa="login-password"]').type(validUser().password);
       cy.get('[data-qa="login-button"]').click();
       cy.validationMessageError('[data-qa="login-email"]');
     });
 
-    it("ST-017 - Should show validation message when password is empty", () => {
+    it("ST-004 - Should show validation message when password is empty", () => {
       cy.get('[data-qa="login-email"]').type(validUser().email);
       cy.get('[data-qa="login-button"]').click();
       cy.validationMessageError('[data-qa="login-password"]');
