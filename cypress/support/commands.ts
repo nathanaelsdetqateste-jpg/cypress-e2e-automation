d81b4af9a6ae9ac28b68b6a3b1152c6d135aeb46
Nathan <nathanael.sdet.qa.teste@gmail.com>
2026-08-08 23:56:58 -0300

    feat: enhance checkout process with card validation and confirmation messages

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,4 +1,6 @@
+import { validCard } from "../factories/card/cardFactory";
 import { validUser } from "../factories/user/userFactory";
+import { cardData } from "../interface/card/cardData";
 import { fillRegistrationForm } from "./helpers/registerHelper";
 
 Cypress.Commands.add("validationMessageError", (selector: string) => {
@@ -41,4 +43,45 @@ Cypress.Commands.add("AddToCart", () => {
     .find("u")
     .should("contain.text", "View Cart")
     .click();
+  cy.url().should("include", "/view_cart");
+});
+
+Cypress.Commands.add("CompleteCheckout", () => {
+  let headersList = [
+    "Item",
+    "Description",
+    "Price",
+    "Quantity",
+    "Total",
+    "Action",
+  ];
+
+  cy.get(".btn.btn-default.check_out").should("be.visible").click();
+
+  cy.url().should("contain", "/checkout");
+
+  for (const header of headersList) {
+    cy.get("#cart_info").find(".cart_menu").should("contain.text", header);
+    break;
+  }
+
+  cy.get(".form-control").type("test");
+  cy.get(".btn.btn-default.check_out").should("be.visible").click();
+
+  cy.get('[data-qa="name-on-card"]').type(validCard().nameCard);
+  cy.get('[data-qa="card-number"]').type(validCard().numberCard);
+  cy.get('[data-qa="cvc"]').type(validCard().cvcCard);
+  cy.get('[data-qa="expiry-month"]').type(
+    validCard().expirationMonthCard.toString(),
+  );
+  cy.get('[data-qa="expiry-year"]').type(
+    validCard().expirationYearCard.toString(),
+  );
+  cy.get('[data-qa="pay-button"]').should("be.visible").click();
+
+  cy.url().should("include", "/payment_done");
+  cy.get("b").should("contains.text", "Order Placed!").and("be.visible");
+  cy.get("p")
+    .should("contains.text", "Congratulations! Your order has been confirmed!")
+    .and("be.visible");
 });
