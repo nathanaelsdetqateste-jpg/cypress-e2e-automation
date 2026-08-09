eb3eb7bd295102fe63a797e00bbc39e8711cbee3
Nathan <nathanael.sdet.qa.teste@gmail.com>
2026-08-09 00:01:06 -0300

    feat: add CompleteCheckout method to Cypress Chainable interface

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -3,5 +3,6 @@ declare namespace Cypress {
     validationMessageError(selector: string): Chainable<string>;
     login(cookieName: string): Chainable<string>;
     AddToCart(): Chainable<string>;
+    CompleteCheckout(): Chainable<string>;
   }
 }
