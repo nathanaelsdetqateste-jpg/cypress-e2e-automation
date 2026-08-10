bcaf875a1d7aea3b3fae25ea5fac123e9099e5e0
Nathan <nathanael.sdet.qa.teste@gmail.com>
2026-08-10 01:29:01 -0300

    refactor: add spacing for improved readability in validation tests

--- cypress/e2e/productComments/validation.cy.ts
+++ cypress/e2e/productComments/validation.cy.ts
@@ -14,6 +14,7 @@ describe("Validation", () => {
         cy.get('input[id="name"]').type(invalidName);
         cy.get('input[id="email"]').type(user.email);
       });
+
       cy.get('textarea[id="review"]').type("This is a test comment.");
 
       cy.get('button[id="button-review"]').click();
@@ -22,13 +23,18 @@ describe("Validation", () => {
 
     it("ST-002 - Add a comment with invalid email format", () => {
       let invalidEmail: string = "invalid-email";
+
       cy.fixture("loginUser").then((user) => {
         cy.get('input[id="name"]').type(user.name);
         cy.get('input[id="email"]').type(invalidEmail);
       });
+
       cy.get('textarea[id="review"]').type("This is a test comment.");
+
       cy.get('button[id="button-review"]').click();
+
       cy.contains("Thank you for your review.").should("not.be.visible");
+
       cy.get('input[id="email"]')
         .invoke("prop", "validationMessage")
         .should("include", invalidEmail);
